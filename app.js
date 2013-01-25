1789dcbff34dca7f927a78273fa91ba74f6a15ec
Christopher De Cairos <chris.decairos@gmail.com>
2013-01-24 20:48:24 -0500

    1. Move routes back into app.js 2. config.json now describes the project, including dependencies 3. responseJS is built better, with dependencies enforced.

--- app.js
+++ app.js
@@ -1,13 +1,81 @@
 'use strict';
-
+console.log( __dirname );
 var express = require( 'express' ),
-    CONFIG = require( './config.json' ),
-    app = express();
+    minify = require( 'uglify-js' ).minify,
+    conf = require( './config.json' ),
+    fs = require( 'fs' ),
+    app = express(),
+    rootJSPath = __dirname + '/' + conf.root + '/',
+    coreJS = fs.readFileSync( rootJSPath + conf.js.core.path, 'UTF-8' ),
+    licenceString = fs.readFileSync( './buildLicense', 'UTF-8' );
 
 app.use( express.logger() );
 app.use( express.static( __dirname + '/public' ) );
 
-require( './routes' )( app, CONFIG );
+
+function getResponse( query ) {
+  var configItem,
+      js = coreJS,
+      loadedDeps = {},
+      minifyCode = false;
+
+  function getItem( js ) {
+    var depends = js.depends;
+
+    if ( depends && !query[ depends ] && !loadedDeps[ depends ] ) {
+      loadedDeps[ depends ] = '';
+      return getItem( conf.js[ depends ] ) + fs.readFileSync( rootJSPath + js.path, 'UTF-8' );
+    }
+
+    return fs.readFileSync( rootJSPath + js.path, 'UTF-8' );
+  }
+
+  try {
+
+    for( var item in query ) {
+      configItem = conf.js[ item ];
+      console.log( configItem );
+      if ( configItem && !loadedDeps[ item ] ) {
+        js += getItem( configItem );
+      } else if ( item === 'minify' ) {
+        minifyCode = true;
+      }
+    }
+
+    if ( minifyCode ) {
+      return licenceString + minify( js, {
+        fromString: true
+      }).code;
+    }
+
+    return licenceString + js;
+  } catch( e ) {
+    console.log( e );
+    return null;
+  }
+}
+
+app.get( '/build', function( req, res ) {
+
+  var responseJS = getResponse( req.query );
+  if ( !responseJS ) {
+    res.status( 500 );
+    res.set({
+      'Content-Type': 'text/plain',
+      'Content-Length': 5
+    });
+    res.send( "Error" );
+  } else {
+    res.status( 200 );
+    res.set({
+      'Content-Type': 'text/javascript',
+      'Content-Length': responseJS.length,
+      'Access-Control-Allow-Origin': "*",
+      'Access-Control-Allow-Methods': "GET"
+    });
+    res.send( responseJS );
+  }
+});
 
 var port = process.env.PORT || 5000;
 app.listen( port );
